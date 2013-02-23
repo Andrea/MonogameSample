7df41ea8d5e7af1d8f85c2a4a6bb464f9a86ce1d
Steven Holdsworth <sholdsworth@gmail.com>
2013-02-23 16:13:47 +0000

    Added iOS Solution and project + Added links to Game.cs & Bloom*.cs

---
+++ BloomPostprocess.iOS/Main.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace BloomPostprocess
+{
+	[Register("AppDelegate")]
+	class Program : UIApplicationDelegate
+	{
+		public override void FinishedLaunching (UIApplication app)
+		{
+			// Fun begins..
+			BloomPostprocessGame game = new BloomPostprocessGame();
+            game.Run();
+		}
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main (string[] args)
+		{
+			UIApplication.Main (args, null, "AppDelegate");
+		}
+	}    
+}
+
+
